048f53872dd8ac567e5cc48af06e463c7fc9f060
Anton Chimirev <anton.chimirev@gmail.com>
2025-09-02 15:05:26 +0300

    Update first.py

--- first.py
+++ first.py
@@ -1,74 +1,82 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import requests
-import io
-import numpy as np
+class AdalineGD:
+    """ADAptive LInear NEuron classifier.
 
-class Perceptron:
+    Parameters
+    ------------
+    eta : float
+      Learning rate (between 0.0 and 1.0)
+    n_iter : int
+      Passes over the training dataset.
+    random_state : int
+      Random number generator seed for random weight
+      initialization.
+
+
+    Attributes
+    -----------
+    w_ : 1d-array
+      Weights after fitting.
+    b_ : Scalar
+      Bias unit after fitting.
+    losses_ : list
+      Mean squared eror loss function values in each epoch.
+
+    """
     def __init__(self, eta=0.01, n_iter=50, random_state=1):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
 
     def fit(self, X, y):
-        """
-        X : array-like, shape = [n_samples, n_features]
-        y : array-like, shape = [n_samples]
+        """ Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+          Training vectors, where n_examples is the number of examples and
+          n_features is the number of features.
+        y : array-like, shape = [n_examples]
+          Target values.
+
+        Returns
+        -------
+        self : object
+
         """
         rgen = np.random.RandomState(self.random_state)
         self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
-        self.b_ = 0.
-        self.errors_ = []
+        self.b_ = np.float_(0.)
+        self.losses_ = []
 
-        for _ in range(self.n_iter):
-            errors = 0
-            for xi, target in zip(X, y):
-                update = self.eta * (target - self.predict(xi))
-                self.w_ += update * xi
-                self.b_ += update
-                errors += int(update != 0.0)
-            self.errors_.append(errors)
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            # Please note that the "activation" method has no effect
+            # in the code since it is simply an identity function. We
+            # could write `output = self.net_input(X)` directly instead.
+            # The purpose of the activation is more conceptual, i.e.,  
+            # in the case of logistic regression (as we will see later), 
+            # we could change it to
+            # a sigmoid function to implement a logistic regression classifier.
+            output = self.activation(net_input)
+            errors = (y - output)
+            
+            #for w_j in range(self.w_.shape[0]):
+            #    self.w_[w_j] += self.eta * (2.0 * (X[:, w_j]*errors)).mean()
+            
+            self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
+            self.b_ += self.eta * 2.0 * errors.mean()
+            loss = (errors**2).mean()
+            self.losses_.append(loss)
         return self
 
     def net_input(self, X):
+        """Calculate net input"""
         return np.dot(X, self.w_) + self.b_
 
+    def activation(self, X):
+        """Compute linear activation"""
+        return X
+
     def predict(self, X):
-        return np.where(self.net_input(X) >= 0.0, 1, 0)
-url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
-response = requests.get(url)
-response.raise_for_status()  # Проверка успешности запроса
-data = response.content.decode('utf-8')
-df = pd.read_csv(io.StringIO(data), header=None)
-print(df.tail())
-
-
-
-y = df.iloc[0:100,4].values
-y = np.where(y == 'Iris-setosa', 0,1)
-
-X = df.iloc[0:100, [0,2]].values
-"""
-plt.scatter(X[:50,0], X[:50, 1], color='red', marker = 'o', label='Setosa')
-
-plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker = 's', label='Versicolor')
-
-plt.xlabel('Длина чашелистика [cм]')
-
-plt.ylabel('Длина лепистка [cм]')
-
-plt.legend(loc='upper left')
-
-plt.show()
-
-
-"""
-
-ppn = Perceptron(eta = 0.1, n_iter = 100)
-ppn.fit(X, y)
-plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker = 'o')
-plt.xlabel("Эпохи")
-plt.ylabel("Количество обновлений")
-
-plt.show()
+        """Return class label after unit step"""
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
